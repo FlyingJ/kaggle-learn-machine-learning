2ed3845b3fa8f315068c36b5d8abe7406da03cf0
J Harrington <jason.m.harrington@gmail.com>
2019-02-05 17:03:30 -0500

    Updates to comply with static code analysis results

--- explore_iris_data.py
+++ explore_iris_data.py
@@ -12,43 +12,44 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
 if __name__ == '__main__':
-    iris_data_file = './data/iris.csv'
-    print('Data file location: {}'.format(iris_data_file))
-    
+    DATA_FILE = './data/iris.csv'
+    print('Data file location: {}'.format(DATA_FILE))
+
     print('Reading data file...')
-    iris_data = pd.read_csv(iris_data_file)
-    
+    DATAFRAME = pd.read_csv(DATA_FILE)
+
     print('Summary information for dataset:')
-    print(iris_data.describe())
-    
+    print(DATAFRAME.describe())
+
     print('Accessing features present in dataset:')
-    print(iris_data.columns)
-    
+    print(DATAFRAME.columns)
+
     print('Saving species information in data vector')
-    y = iris_data.Name
-    
+    CLASS = 'Name'
+    Y = DATAFRAME[CLASS]
+
     print('Saving species feature observations to dataframe')
-    iris_features = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
-    
-    X = iris_data[iris_features]
-    
+    FEATURES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
+
+    X = DATAFRAME[FEATURES]
+
     print('Summary information for data features:')
     print(X.describe())
     print('Display first 5 observations from feature dataframe')
     print(X.head())
-    
+
     # split data into training, validation sets
     # static random state for reproducibility
     print('Split features and labels into training and validation sets')
-    train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
-    
+    TRAIN_X, VAL_X, TRAIN_Y, VAL_Y = train_test_split(X, Y, random_state=1)
+
     # create classifier model
     print('Create decision tree classification model')
-    iris_model = DecisionTreeClassifier(random_state=1)
-    iris_model.fit(train_X, train_y)
-    
+    MODEL = DecisionTreeClassifier(random_state=1)
+    MODEL.fit(TRAIN_X, TRAIN_Y)
+
     print('Use model to predict species names from features in validation set')
-    val_predictions = iris_model.predict(val_X)
+    VAL_PREDICTIONS = MODEL.predict(VAL_X)
+    MODEL_ACCURACY = accuracy_score(VAL_Y, VAL_PREDICTIONS)
     print('Compare predictions vs validation set labels.')
-    print('Accuracy of model using validation data: {}'.format(accuracy_score(val_y, val_predictions)))
-    
+    print('Accuracy of model using validation data: {}'.format(MODEL_ACCURACY))
